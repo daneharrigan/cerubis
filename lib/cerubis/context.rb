1d461de1d246cfc9ca112243fa8f91df8d487b64
Dane Harrigan <dane.harrigan@gmail.com>
2011-07-30 20:44:34 -0400

    context object returns true, false, integers, floats and items from context

--- lib/cerubis/context.rb
+++ lib/cerubis/context.rb
@@ -1,8 +1,25 @@
 class Cerubis
   class Context < Hash
-    def initialize(hash)
+    def initialize(hash={})
       super(nil)
       hash.each { |key, value| self[key] = value }
     end
+
+		def get(key)
+			case key
+				when /^true$/   then true
+				when /^false$/  then false
+				when /^[0-9]+$/ then Integer(key)
+				when /^[0-9]+\.[0-9]+$/ then Float(key)
+				else
+					object_methods = key.split('.')
+					object = self[object_methods.shift.to_sym]
+					while meth = object_methods.shift do
+						object = object.respond_to?(meth) ? object.send(meth) : nil
+					end
+
+					object
+			end
+		end
   end
 end
---
+++ test/cerubis/context_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class Cerubis::ContextTest < MiniTest::Unit::TestCase
+	def test_context_returns_true
+		context = Cerubis::Context.new
+		assert_equal true, context.get('true')
+	end
+
+	def test_context_returns_false
+		context = Cerubis::Context.new
+		assert_equal false, context.get('false')
+	end
+
+	def test_context_returns_integer
+		context = Cerubis::Context.new
+		assert_equal 1, context.get('1')
+	end
+
+	def test_context_returns_float
+		context = Cerubis::Context.new
+		assert_equal 1.5, context.get('1.5')
+	end
+
+	def test_context_returns_item_from_context
+		context = Cerubis::Context.new(item: :foo)
+		assert_equal :foo, context.get('item')
+	end
+end
