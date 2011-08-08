b2dc2879bcc724fd181d92f832d66d089f6ecf0b
Dane Harrigan <dane.harrigan@gmail.com>
2011-08-08 00:31:12 -0400

    changed test names

--- test/matchers/test_variable.rb
+++ test/matchers/test_variable.rb
@@ -1,25 +1,29 @@
 module TestVariable
-  def test_output_object
+  def test_variable_object
     assert_match '{{object}}', Cerubis::Matcher::Variable
   end
 
-  def test_output_object_with_spaces
+  def test_variable_object_with_spaces
     assert_match '{{ object }}', Cerubis::Matcher::Variable
   end
 
-  def test_output_object_attribute
+  def test_variable_object_attribute
     assert_match '{{object.attribute}}', Cerubis::Matcher::Variable
   end
 
-  def test_output_object_attribute_with_spaces
+  def test_variable_object_attribute_with_spaces
     assert_match '{{ object.attribute }}', Cerubis::Matcher::Variable
   end
 
-  def test_output_object_attribute_chain
+  def test_variable_object_attribute_chain
     assert_match '{{object.attribute.attr2}}', Cerubis::Matcher::Variable
   end
 
-  def test_output_object_attribute_chain_with_spaces
+  def test_variable_object_attribute_chain_with_spaces
     assert_match '{{ object.attribute.attr2 }}', Cerubis::Matcher::Variable
+  end
+
+  def test_helper_with_object
+    assert_match '{{ helper(object) }}', Cerubis::Matcher::Variable
   end
 end
