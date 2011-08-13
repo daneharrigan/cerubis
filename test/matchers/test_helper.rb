e3e3db28a2ad52c2a1a9ea2bf633395c74934581
Dane Harrigan <dane.harrigan@gmail.com>
2011-08-13 13:54:01 -0700

    tested more helper use cases

--- test/matchers/test_helper.rb
+++ test/matchers/test_helper.rb
@@ -1,17 +1,37 @@
 module TestHelper
-  def test_one_helper_with_one_object
+  def test_helper_with_one_object
     assert_match Cerubis::Matcher::Helper, 'helper(obj)'
   end
 
-  def test_one_helper_with_two_objects
+  def test_helper_with_one_object_and_method
+    assert_match Cerubis::Matcher::Helper, 'helper(obj.method)'
+  end
+
+  def test_helper_with_two_objects
     assert_match Cerubis::Matcher::Helper, 'helper(obj, 1)'
   end
 
-  def test_one_helper_with_multiple_objects
+  def test_helper_with_multiple_objects
     assert_match Cerubis::Matcher::Helper, 'helper(obj, 1, foo)'
   end
 
-  def test_one_helper_with_two_objects_one_with_quotes
+  def test_helper_with_two_objects_one_with_quotes
     assert_match Cerubis::Matcher::Helper, "helper(obj, 'foo')"
+  end
+
+  def test_helper_with_nested_helper
+    assert_match Cerubis::Matcher::Helper, 'helper(helper(obj))'
+  end
+
+  def test_helper_with_object_and_nested_helper
+    assert_match Cerubis::Matcher::Helper, 'helper(obj, helper(obj))'
+  end
+
+  def test_helper_with_string_and_nested_helper
+    assert_match Cerubis::Matcher::Helper, "helper('foo', helper(obj))"
+  end
+
+  def test_helper_with_two_nested_helpers
+    assert_match Cerubis::Matcher::Helper, "helper(helper(obj), helper(obj))"
   end
 end
