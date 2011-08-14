af8ac3bcab17b45932d20f11255a5233715f0832
Dane Harrigan <dane.harrigan@gmail.com>
2011-08-14 10:25:50 -0700

    moved full rendering tests to rendered_test file

--- test/cerubis/template_test.rb
+++ test/cerubis/template_test.rb
@@ -25,40 +25,6 @@ class Cerubis::TemplateTest < MiniTest::Unit::TestCase
     assert_equal '</body>', template.nodes[2].render.strip
   end
 
-  def test_parse_single_block
-    template = Cerubis::Template.new(content)
-    html = Capybara::Node::Simple.new(template.to_html)
-
-    assert html.has_selector?('body > section', text: 'Foo Content')
-  end
-
-  def test_parse_nested_blocks
-    template = Cerubis::Template.new(nested_content)
-    html = Capybara::Node::Simple.new(template.to_html)
-
-    assert html.has_selector?('body > header > h1', text: 'Header Content')
-    assert html.has_selector?('body > header > section > p', text: 'Paragraph Content')
-
-    refute html.has_selector?('body > header > #hidden', text: 'Hidden Content')
-    refute_match /\{\{/, template.to_html
-  end
-
-  def test_parse_unless_block
-    template = Cerubis::Template.new(hidden_content)
-    html = Capybara::Node::Simple.new(template.to_html)
-
-    refute html.has_selector?('body > #hidden', text: 'Hidden Content')
-    refute_match /\{\{/, template.to_html
-  end
-
-  def test_parse_loop_block
-    template = Cerubis::Template.new(loop_content, collection: [1,2,3])
-    html = Capybara::Node::Simple.new(template.to_html)
-
-    assert html.has_selector?('body > p', text: 'Loop Content', count: 3)
-    refute_match /\{\{/, template.to_html
-  end
-
   private
     def content
       <<-STR
@@ -67,45 +33,6 @@ class Cerubis::TemplateTest < MiniTest::Unit::TestCase
           <section>Foo Content</section>
         {{/if}}
         </body>
-      STR
-    end
-
-    def nested_content
-      <<-STR
-        <body>
-          <header>
-          {{#if true}}
-            <h1>Header Content</h1>
-            {{#if true}}
-              <section>
-                {{#if true}}
-                  <p>{{#if true}} Paragraph Content {{/if}}</p>
-                {{/if}}
-              </section>
-            {{/if}}
-          {{/if}}
-          </header>
-        </body>
-      STR
-    end
-
-    def hidden_content
-      <<-STR
-				<body>
-        {{#unless true}}
-          <section id="hidden">Hidden Content</section>
-        {{/unless}}
-				</body>
-      STR
-    end
-
-    def loop_content
-      <<-STR
-      <body>
-      {{#loop item in collection}}
-        <p>Loop Content</p>
-      {{/loop}}
-      </body>
       STR
     end
 end
